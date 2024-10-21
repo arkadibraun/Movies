f2818c23f045809f7a6dd6e1a9dd5a7d3b1b3e55
Arkadi Braun <arkadi.braun001@stud.fh-dortmund.de>
2024-10-21 16:30:01 +0200

    first commit

---
+++ movies/src/main/java/test/movies/Movie.java
@@ -0,0 +1,20 @@
+package test.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collation = "movies")
+public class Movie {
+    private ObjectId id;
+    private String imbdId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+
+    private List<String> genres;
+    private List<String> backdrops;
+
+}
